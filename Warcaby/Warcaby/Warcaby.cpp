7ad72c85784d15f19f7c7f06a9d6879b5bd7716b
DiamondHoe <desineverbum@gmail.com>
2021-05-08 15:40:40 +0200

    Zmiany wizualne planszy oraz nazw zmiennych

--- Warcaby/Warcaby/Warcaby.cpp
+++ Warcaby/Warcaby/Warcaby.cpp
@@ -22,16 +22,16 @@ int main()
 {
 Begin:
 
-
     int i, j;
-    char Board[8][8], A, B, pozycja[2];
+    string quit;
+    char Plansza[8][8], A, B, pozycja[2];
     for (i = 0; i < 8; i++)
     {
         for (j = 0; j < 8; j++)
-            Board[i][j] = ' ';
+            Plansza[i][j] = ' ';
     }
-    cout << "\t\t\t" << "Witaj w grze w warcaby!" << endl;
-    cout << "Zasady sa proste:" << endl << endl;
+    cout << "Witaj w grze w warcaby!" << endl;
+    cout << "Zasady sa proste:" << endl;
     cout << "Kto straci wszystkie pionki przegrywa!" << endl << endl;
     cout << " !    @    #    $    %    ^    &    *    (    )    /    -    +    = " << endl << endl;
 
@@ -39,7 +39,7 @@ Begin:
     cout << "Jaki symbol ma miec przeciwnik? "; cin >> B;
     if (A == B)
     {
-        cout << endl << "\t\t" << "Nie mozna wybrać tych samych symboli";
+        cout << endl << "\t\t" << "Nie mozna wybrać tych samych symboli"<<endl<<endl;
 
         goto Begin;
     }
@@ -50,15 +50,19 @@ Begin:
 
 
 
-    Board[0][0] = B; Board[0][2] = B; Board[0][4] = B; Board[0][6] = B;
-    Board[1][1] = B; Board[1][3] = B; Board[1][5] = B; Board[1][7] = B;
-    Board[2][0] = B; Board[2][2] = B; Board[2][4] = B; Board[2][6] = B;
+    Plansza[0][0] = B; Plansza[0][2] = B; Plansza[0][4] = B; Plansza[0][6] = B;
+    Plansza[1][1] = B; Plansza[1][3] = B; Plansza[1][5] = B; Plansza[1][7] = B;
+    Plansza[2][0] = B; Plansza[2][2] = B; Plansza[2][4] = B; Plansza[2][6] = B;
 
-    Board[5][1] = A; Board[5][3] = A; Board[5][5] = A; Board[5][7] = A;
-    Board[6][0] = A; Board[6][2] = A; Board[6][4] = A; Board[6][6] = A;
-    Board[7][1] = A; Board[7][3] = A; Board[7][5] = A; Board[7][7] = A;
+    Plansza[5][1] = A; Plansza[5][3] = A; Plansza[5][5] = A; Plansza[5][7] = A;
+    Plansza[6][0] = A; Plansza[6][2] = A; Plansza[6][4] = A; Plansza[6][6] = A;
+    Plansza[7][1] = A; Plansza[7][3] = A; Plansza[7][5] = A; Plansza[7][7] = A;
 
-    Display(Board);
-
-    return 0;
+    Display(Plansza);
+    
+    cout << "Zakonczyc?";
+    cin >> quit;
+    if (quit == "t" || "tak" || "ta") { return 0; }
+    else { goto Begin; };
+   
 }
