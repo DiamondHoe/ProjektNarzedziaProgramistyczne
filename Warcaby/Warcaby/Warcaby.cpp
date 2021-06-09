16d11d34a558a5d9ac944b5c1e72223914b6f3cb
Pawlonix <pawelek.2611@wp.pl>
2021-06-09 21:36:24 +0200

    Dodanie ruchu przeciwnika oraz pętli rozgrywki

--- Warcaby/Warcaby/Warcaby.cpp
+++ Warcaby/Warcaby/Warcaby.cpp
@@ -330,11 +330,283 @@ int main()
                 gracz2++;
         }
     }
+    goto start2;
+done2:
+    Display(Plansza);
+
+    gracz1 = 0; gracz2 = 0;
+    A = sym1p;
+    B = sym2p;
+    for (i = 0; i < 8; i++)
+    {
+        for (j = 0; j < 8; j++)
+        {
+            if (Plansza[i][j] == A)
+                gracz2++;
+        }
+    }
+    for (i = 0; i < 8; i++)
+    {
+        for (j = 0; j < 8; j++)
+        {
+            if (Plansza[i][j] == B
+)
+                gracz1++;
+        }
+    }
+    goto start;
    
         
 
     
     cout << endl << "Ruch przeciwnika (" << B << ") Tura........" << endl;
+   start2: 
+       Display(Plansza);
+    cout << "Twoja (" << B << ") Tura";
+
+    cout << endl << "Wybierz pozycje: ( Taka jak: A1, G6, C3 itd. ) ";
+    cin >> (pozycja);
+    if (pozycja[0] == 'A')
+        X = 0;
+    else if (pozycja[0] == 'B')
+        X = 1;
+    else if (pozycja[0] == 'C')
+        X = 2;
+    else if (pozycja[0] == 'D')
+        X = 3;
+    else if (pozycja[0] == 'E')
+        X = 4;
+    else if (pozycja[0] == 'F')
+        X = 5;
+    else if (pozycja[0] == 'G')
+        X = 6;
+    else if (pozycja[0] == 'H')
+        X = 7;
+    else
+    {
+        cout << "Wpisz ponownie..." << endl;
+
+        goto start2;
+    }
+    if (pozycja[1] == '1')
+        Y = 0;
+    else if (pozycja[1] == '2')
+        Y = 1;
+    else if (pozycja[1] == '3')
+        Y = 2;
+    else if (pozycja[1] == '4')
+        Y = 3;
+    else if (pozycja[1] == '5')
+        Y = 4;
+    else if (pozycja[1] == '6')
+        Y = 5;
+    else if (pozycja[1] == '7')
+        Y = 6;
+    else if (pozycja[1] == '8')
+        Y = 7;
+    else
+    {
+        cout << "Wpisz ponownie..." << endl;
+
+        goto start2;
+    }
+    if (Plansza[Y][X] != B)
+    {
+        cout << "Wpisz ponownie..." << endl;
+
+        goto start2;
+    }
+
+
+
+    cout << endl;
+    cout << "W jakim kierunku ruszyc? " << endl;
+    cout << "1 (Lewy gorny rog) , 2 (Prawy gorny rog) " << endl;
+    cout << "3 (Lewy dolny rog) , 4 (Prawy dolny rog) "; cin >> ruch;
+    A = sym1p;
+    B = sym2p;
+    if (ruch == 1)
+    {
+        if (X == 0 || Y == 0)
+        {
+            cout << "Token can't move...." << endl;
+
+            goto start2;
+        }
+        if (Plansza[Y - 1][X - 1] == B)
+        {
+            cout << "Token can't move...." << endl;
+
+            goto start2;
+        }
+        if (Plansza[Y - 1][X - 1] == ' ')
+        {
+            Plansza[Y][X] = ' ';
+            Plansza[--Y][--X] = B;
+            goto done2;
+        }
+        if (Plansza[Y - 1][X - 1] == A)
+        {
+            if (X <= 1)
+            {
+                cout << "Token can't move...." << endl;
+
+                goto start2;
+            }
+            if (Plansza[Y - 2][X - 2] != ' ')
+            {
+                cout << "Token can't move...." << endl;
+
+                goto start2;
+            }
+            else
+            {
+                Plansza[Y][X] = ' ';
+                Plansza[Y - 1][X - 1] = ' ';
+                Y -= 2; X -= 2;
+                Plansza[Y][X] = B;
+                goto done2;
+            }
+        }
+    }
+
+    // UR Diagonal
+    if (ruch == 2)
+    {
+        if (X == 7 || Y == 0)
+        {
+            cout << "Token can't move...." << endl;
+
+            goto start2;
+        }
+        if (Plansza[Y - 1][X + 1] == B)
+        {
+            cout << "Token can't move...." << endl;
+
+            goto start2;
+        }
+        if (Plansza[Y - 1][X + 1] == ' ')
+        {
+            Plansza[Y][X] = ' ';
+            Plansza[--Y][++X] = B;
+            goto done2;
+        }
+        if (Plansza[Y - 1][X + 1] == A)
+        {
+            if (X >= 6)
+            {
+                cout << "Token can't move...." << endl;
+
+                goto start2;
+            }
+            if (Plansza[Y - 2][X + 2] != ' ')
+            {
+                cout << "Token can't move...." << endl;
+
+                goto start2;
+            }
+            else
+            {
+                Plansza[Y][X] = ' ';
+                Plansza[Y - 1][X + 1] = ' ';
+                Y -= 2; X += 2;
+                Plansza[Y][X] = B;
+                goto done2;
+            }
+        }
+    }
+
+    // DL Diagonal
+    if (ruch == 3)
+    {
+        if (X == 0 || Y == 7)
+        {
+            cout << "Token can't move...." << endl;
+
+            goto start2;
+        }
+        if (Plansza[Y + 1][X - 1] == B)
+        {
+            cout << "Token can't move...." << endl;
+
+            goto start2;
+        }
+        if (Plansza[Y + 1][X - 1] == ' ')
+        {
+            Plansza[Y][X] = ' ';
+            Plansza[++Y][--X] = B;
+            goto done2;
+        }
+        if (Plansza[Y + 1][X - 1] == A)
+        {
+            if (X <= 1)
+            {
+                cout << "Token can't move...." << endl;
+
+                goto start2;
+            }
+            if (Plansza[Y + 2][X - 2] != ' ')
+            {
+                cout << "Token can't move...." << endl;
+
+                goto start2;
+            }
+            else
+            {
+                Plansza[Y][X] = ' ';
+                Plansza[Y + 1][X - 1] = ' ';
+                Y += 2; X -= 2;
+                Plansza[Y][X] = B;
+                goto done2;
+            }
+        }
+    }
+
+
+    if (ruch == 4)
+    {
+        if (X == 7 || Y == 7)
+        {
+            cout << "Token can't move...." << endl;
+
+            goto start2;
+        }
+        if (Plansza[Y + 1][X + 1] == B)
+        {
+            cout << "Token can't move...." << endl;
+
+            goto start2;
+        }
+        if (Plansza[Y + 1][X + 1] == ' ')
+        {
+            Plansza[Y][X] = ' ';
+            Plansza[++Y][++X] = B;
+            goto done2;
+        }
+        if (Plansza[Y + 1][X + 1] == A)
+        {
+            if (X >= 6)
+            {
+                cout << "Token can't move...." << endl;
+
+                goto start2;
+            }
+            if (Plansza[Y + 2][X + 2] != ' ')
+            {
+                cout << "Token can't move...." << endl;
+
+                goto start2;
+            }
+            else
+            {
+                Plansza[Y][X] = ' ';
+                Plansza[Y + 1][X + 1] = ' ';
+                Y += 2; X += 2;
+                Plansza[Y][X] = B;
+                goto done2;
+            }
+        }
+    }
 
 
 
